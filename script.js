5cf8b81f53421d04ab1c888651ab066107647aff
nishchal singh <singhnishchal27@gmail.com>
2024-10-29 16:08:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function Person(name, age) {
     }
     
     greet() {
-      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+      console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
     }
   }
 
