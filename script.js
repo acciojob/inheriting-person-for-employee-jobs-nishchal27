61dc4d5a541f918cc028d76634e5f21d49455939
nishchal singh <singhnishchal27@gmail.com>
2024-10-29 16:06:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
 // complete this js code
 function Person(name, age) {
-	class Person {
-		constructor(name, age){
-			this.name = name;
-			this.age = age;
-		}
-		greet(){
-			console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`)
-		}
-	}
+	  // Define the Person class inside the function
+  class PersonClass {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
+    }
+    
+    greet() {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+    }
+  }
+
+  // Return an instance of PersonClass
+  return new PersonClass(name, age);
 }
 
 function Employee(name, age, jobTitle) {
-	class Employee extends Person {
-		constructor(name, age, jobTitle){
-			super(name, age);
-			this.jobTitle = jobTitle;
-		}
+	  // Define the Employee class inside the function
+  class EmployeeClass extends Person(name, age).constructor {
+    constructor(name, age, jobTitle) {
+      super(name, age);
+      this.jobTitle = jobTitle;
+    }
 
-		jobGreet(){
-			console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
-		}
-	}
+    jobGreet() {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+    }
+  }
+
+  // Return an instance of EmployeeClass
+  return new EmployeeClass(name, age, jobTitle);
 }
 
 // Do not change code below this line
